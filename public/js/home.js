b782e788fae043e91f680b9bde5e26435b1e9833
Lotan <bscvcibsc@gmail.com>
2024-11-07 21:45:27 +0200

    Fix navbar logout

--- public/js/home.js
+++ public/js/home.js
@@ -73,7 +73,7 @@ $(document).ready(function () {
 
     function renderBooks() {
         let html = '';
-        const booksContainer = $('.books-continer');
+        const booksContainer = $('.books-container');
 
         const filteredBooks = books.filter(book => {
             const matchesAuthor = filters.authors.size === 0 || filters.authors.has(book.author);
@@ -107,52 +107,22 @@ $(document).ready(function () {
 
         $(".book-card").each(function () {
             let title = $(this).find(".card-title").text().toLowerCase();
-            let author = $(this).find(".card-text").first().text().toLowerCase();
-            let price = $(this).find(".card-text").first().text().toLowerCase();
-
+            let author = $(this).find(".book-author").text().toLowerCase();
 
             if (title.includes(searchTerm) || author.includes(searchTerm)) {
-                $(this).show();  
+                $(this).closest('a').show();  
             } else {
-                $(this).hide(); 
+                $(this).closest('a').hide(); 
             }
         });
     });
-
-    function renderBooks() {
-        console.log(books);
-        let html = ''
-        booksContainer = $('.books-continer')
-
-        $.each(books, function (index, book) { 
-            console.log(book.author);
-            html += `
-            <div class="card book-card col-2 me-4 mb-4">
-                <img src="/api/placeholder/400/300" class="card-img-top" alt="${book.title}">
-                <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
-                    <p class="card-text">Author: ${book.author}</p>
-                    <p class="card-text">Year: ${book.year}</p>
-                    <p>Price: $${book.price}</p>
-                    <button class="add-to-cart-btn" data-price=${book.price} data-title="${book.title}" data-author="${book.author}">Add to Cart</button>
-
-                </div>
-            </div>
-            `
-        });
-        booksContainer.html(html)
-
-    }
 
     $(document).on('click', '.add-to-cart-btn', function () {
         const title = $(this).data('title');
         const author = $(this).data('author');
         const price = $(this).data('price');
 
-
         let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-        console.log('Cart before adding:', cart);
 
         cart.push({ title, author, price });
 
